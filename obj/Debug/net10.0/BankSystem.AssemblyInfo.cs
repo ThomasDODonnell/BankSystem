08b29e1f9c264ea90c1fa51694be09ef70c49fd7
ThomasDODonnell <159654168+ThomasDODonnell@users.noreply.github.com>
2026-01-28 16:34:47 +0000

    fixed docker compose misspelling

--- obj/Debug/net10.0/BankSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/BankSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17bdd2443d39a98be55babcf20140a4e4ec142b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a27492195a8a468767612ed7e8233dd4eebda8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
